79e87c3a58c18156a19535d52c719252f6bf5c50
remiscarlet <remilia@scarl.et>
2022-07-06 00:19:48 +0000

    Better default config search

--- config.py
+++ config.py
@@ -55,15 +55,33 @@ class Config(ConfigNode):
     data: Dict
     config_path: Path
 
+    default_config_name: str = "patchouli_config.yml"
+
     def __init__(self, config_path: Optional[Path] = None):
         self.config_path = (
-            config_path if config_path is not None else Path("config.yml")
+            config_path if config_path is not None else self.get_default_config()
         )
 
         with open(self.config_path, "r") as f:
             self.data = yaml.safe_load(f)
 
         super().__init__(self.data)
+
+    def get_default_config(self, curr_dir: Optional[Path] = None):
+        if str(curr_dir) == "/":
+            raise FileNotFoundError(
+                f"Could not find a valid {self.default_config_name}."
+            )
+
+        if curr_dir is None:
+            curr_dir = Path(__file__)
+
+        # Assumes config.yml is either in currdir or in a ancestor dir.
+        config_path = curr_dir / self.default_config_name
+        if config_path.exists():
+            return config_path
+        else:
+            return self.get_default_config(curr_dir.parent)
 
     def printconfig(self):
         pprint(self.data)
