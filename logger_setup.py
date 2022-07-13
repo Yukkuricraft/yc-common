e3f1b4de519a89671443d7189af003dcbbb1993c
remiscarlet <remilia@scarl.et>
2022-07-13 23:45:49 +0000

    Add logger_setup

---
+++ logger_setup.py
@@ -0,0 +1,12 @@
+import logging
+
+logger: logging.Logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter: logging.Formatter = logging.Formatter("[%(asctime)s][%(levelname)s] %(message)s")
+
+ch: logging.StreamHandler = logging.StreamHandler()
+ch.setFormatter(formatter)
+ch.setLevel(logging.DEBUG)
+
+logger.addHandler(ch)
