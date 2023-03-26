dcd5e3ead5cefa6d0a7005c17f2c565d91bf589c
remiscarlet <remilia@scarl.et>
2023-03-26 22:04:42 +0000

    Add recursive_chown/chmod

--- helpers.py
+++ helpers.py
@@ -1,9 +1,13 @@
 import shutil
 import os
+from typing import Optional
 
 from src.common.logger_setup import logger
 
-def recursive_chown(path, uid, gid):
+def recursive_chown(path, uid: Optional[int] = None, gid: Optional[int] = None):
+    if uid is None and gid is None:
+      raise ValueError(f"You must provide a UID, GID, or both but not")
+
     logger.info(">> RECURSIVELY CHOWNING ")
     logger.info(path)
     for dirpath, _, filenames in os.walk(path):
@@ -11,3 +15,14 @@ def recursive_chown(path, uid, gid):
         shutil.chown(dirpath, uid, gid)
         for filename in filenames:
             shutil.chown(os.path.join(dirpath, filename), uid, gid)
+
+def recursive_chmod(path, mode: int):
+    # Should really validate mode.
+
+    logger.info(">> RECURSIVELY CHMODDING ")
+    logger.info(path)
+    for dirpath, _, filenames in os.walk(path):
+        logger.info(dirpath)
+        os.chmod(dirpath, mode)
+        for filename in filenames:
+            os.chmod(os.path.join(dirpath, filename), mode)
