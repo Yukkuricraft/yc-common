f1539f986791adfbf4b53663e0ae93533ec8bbeb
remiscarlet <remilia@scarl.et>
2023-12-26 19:02:05 -0500

    Add EnvConfig

--- config/__init__.py
+++ config/__init__.py
@@ -8,6 +8,7 @@ from typing import Optional, Dict, List, Tuple
 
 from src.common.config.config_finder import ConfigFinder
 from src.common.config.config_node import ConfigNode
+from src.common.config.env_config import EnvConfig
 from src.common.config.yaml_config import YamlConfig
 from src.common.config.toml_config import TomlConfig
 
@@ -15,7 +16,6 @@ from src.common.config.toml_config import TomlConfig
 __TOML_CONFIG: dict = {}
 __YAML_CONFIG: dict = {}
 __ENV_CONFIG: dict = {}
-
 
 def load_toml_config(
     config_name: Optional[str] = None,
@@ -46,18 +46,10 @@ def load_env_config(
     global __ENV_CONFIG
 
     if config_name not in __ENV_CONFIG or __ENV_CONFIG[config_name] is None or no_cache:
-        path = ConfigFinder(config_name, base_path).config_path
-        config: Dict = {}
-        with open(path, "r") as f:
-            for line in f.readlines():
-                line = line.strip()
-                if not line:
-                    continue
-
-                key, value = line.split("=")
-                config[key] = value
-
-        __ENV_CONFIG[config_name] = config
+        __ENV_CONFIG[config_name] = EnvConfig(
+            config_name,
+            base_path
+        )
 
     return __ENV_CONFIG[config_name]
 
---
+++ config/env_config.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+from typing import TextIO, Dict, Optional
+from pathlib import Path
+from configparser import ConfigParser
+from pprint import pprint, pformat
+
+from src.common.config.config_node import ConfigNode
+from src.common.config.config_finder import ConfigFinder
+from src.common.logger_setup import logger
+
+
+class EnvConfig(ConfigNode):
+    data: Dict
+    config_path: Path
+
+    def __init__(
+        self, config_path: Optional[str] = None, base_path: Optional[Path] = None
+    ):
+        self.config_path = ConfigFinder(config_path, base_path).config_path
+
+        self.data = {}
+        with open(self.config_path, "r") as f:
+            for line in f.readlines():
+                line = line.strip()
+                if not line:
+                    continue
+                split = line.split("=")
+                key = split[0]
+                val = "=".join(split[1:]) # Just incase there were any ='s in the vals...? Is that even valid syntax?
+
+                self.data[key] = val
+
+        if self.data == None:
+            self.data = {}
+
+        logger.info(self.config_path)
+        logger.info(self.data)
+
+        super().__init__(self.data)
+
+    def print_config(self):
+        pprint(self.data)
+
+    @staticmethod
+    def write_cb(f: TextIO, config: Dict, quote: Optional[bool] = True):
+        """Env config write callback.
+
+        Args:
+            f (TextIO): File object to write to
+            config (dict): Config to dump
+            quote (Optional[bool]) Whether to quote the config vals. Defaults to True
+        """
+        logger.info(pformat(config))
+        for key, value in config.items():
+            if quote:
+                f.write(f'{key}="{value}"\n'.encode("utf8"))
+            else:
+                f.write(f'{key}={value}\n'.encode("utf8"))
