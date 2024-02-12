de0429fe41a69074d0b9c672728e2227e55b27da
remiscarlet <remilia@scarl.et>
2024-02-12 01:01:10 -0500

    Move label consts into common. Add HOST_REPO_ROOT_PATH

--- constants.py
+++ constants.py
@@ -1,9 +1,11 @@
+import os
 import stat
 from pathlib import Path
 from typing import Any
 
 BASE_DATA_PATH = Path("/var/lib/yukkuricraft")
 REPO_ROOT_PATH = Path(__file__).parent.parent.parent  # G w o s s
+HOST_REPO_ROOT_PATH = Path(os.getenv("HOST_YC_REPO_ROOT", "/couldnotfindavalidhostrepopath/"))
 
 VELOCITY_FORWARDING_SECRET_PATH: Path = (
     REPO_ROOT_PATH / "secrets" / "velocity" / "forwarding.secret"
@@ -19,3 +21,9 @@ DEFAULT_CHMOD_MODE = (
     | stat.S_IROTH
     | stat.S_IXOTH
 )
+
+YC_CONTAINER_TYPE_LABEL = "net.yukkuricraft.container_type"
+YC_CONTAINER_NAME_LABEL = "net.yukkuricraft.container_name"
+YC_ENV_LABEL = "net.yukkuricraft.env"
+
+
